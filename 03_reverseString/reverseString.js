4b8daa35618b228b470f536439fc38bd795f72fc
Anudeep <160405043+anewdeep@users.noreply.github.com>
2025-03-11 20:27:37 +0530

    feat: complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const strArr = input.split("");
+    let reverse = '';
+    for (let i = strArr.length - 1; i >= 0; i--)
+        reverse += strArr[i];
+    return reverse;
 };
 
 // Do not edit below this line
