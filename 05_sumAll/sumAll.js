c81a478aac80acc1c1ff7f36f9e85bcaaed74c43
Anudeep <160405043+anewdeep@users.noreply.github.com>
2025-03-12 16:43:39 +0530

    feat: complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (!Number.isInteger(first) || !Number.isInteger(last)) return 'ERROR';
+    if (first < 0 || last < 0) return 'ERROR'
+    if (first > last) [first, last] = [last, first];
+    let sum = 0;
+    let i = first;
+    while (i <= last) {
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
